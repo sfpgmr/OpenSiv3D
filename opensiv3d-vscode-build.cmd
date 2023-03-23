0fb6d8b41b0b488d2d10ab08a47f991e4fe3b6ec
sfpgmr <sfpgmr@dummy.com>
2023-03-24 05:46:22 +0900

    update

--- opensiv3d-vscode-build.cmd
+++ opensiv3d-vscode-build.cmd
@@ -2,4 +2,5 @@ setlocal
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 MSBuild.exe .\windowsDesktop\OpenSiv3D.sln 
 MSBuild.exe .\windowsDesktop\OpenSiv3D.sln /t:Build /p:Configuration=Release /p:Platform=x64
+rem MSBuild.exe .\windowsDesktop\OpenSiv3D.sln /t:Build /p:Configuration=Debug /p:Platform=x64
 endlocal
